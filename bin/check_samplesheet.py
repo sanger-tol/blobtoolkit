5c521ebe633c172a5aec64502ae25cf22f2e0ee1
alxndrdiaz <ra.ramos.diaz@gmail.com>
2022-08-22 08:23:03 -0600

    restored script

--- bin/check_samplesheet.py
+++ bin/check_samplesheet.py
@@ -1,7 +1,6 @@
 #!/usr/bin/env python3
 
 # This script is based on the example at: https://raw.githubusercontent.com/nf-core/test-datasets/viralrecon/samplesheet/samplesheet_test_illumina_amplicon.csv
-# should check column with masked fasta files instead of cram files
 
 import os
 import sys
@@ -16,6 +15,7 @@ def parse_args(args=None):
     parser = argparse.ArgumentParser(description=Description, epilog=Epilog)
     parser.add_argument("FILE_IN", help="Input samplesheet file.")
     parser.add_argument("FILE_OUT", help="Output file.")
+    parser.add_argument('--version', action='version', version='%(prog)s 1.0')
     return parser.parse_args(args)
 
 
@@ -36,13 +36,11 @@ def print_error(error, context="Line", context_str=""):
 def check_samplesheet(file_in, file_out):
     """
     This function checks that the samplesheet follows the following structure:
-
     sample,datatype,datafile
     sample1,hic,/path/to/file1.cram
     sample1,illumina,/path/to/file2.cram
     sample1,pacbio,/path/to/file3.cram
     sample1,ont,/path/to/file4.cram
-
     For an example see:
     https://raw.githubusercontent.com/nf-core/test-datasets/viralrecon/samplesheet/samplesheet_test_illumina_amplicon.csv
     """
@@ -84,7 +82,7 @@ def check_samplesheet(file_in, file_out):
                 print_error("Sample entry has not been specified!", "Line", line)
 
             ##* Check datatype name entries
-            datatypes = ["hic", "pacbio", "illumina", "ont", "pacbio_clr"]
+            datatypes = ["hic"]
             if datatype:
                 if datatype not in datatypes:
                     print_error(
