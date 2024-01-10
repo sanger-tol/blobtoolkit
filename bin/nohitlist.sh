04ea5e333e022d858208422e2c1d01d3192b2813
Matthieu Muffato <mm49@sanger.ac.uk>
2024-01-10 15:03:24 +0000

    bugfix: the headers are separated with regular whitespace, not tabs

--- bin/nohitlist.sh
+++ bin/nohitlist.sh
@@ -9,7 +9,7 @@ E=$4
 # find ids of sequences with no hits in the blastx search
 grep '>' $fasta | \
     grep -v -w -f <(awk -v evalue="$E" '{if($14<evalue){print $1}}' $blast | sort | uniq) | \
-    cut -f1 | sed 's/>//' > $prefix.nohit.txt
+    awk '{print $1}' | sed 's/>//' > $prefix.nohit.txt
 
 
 
