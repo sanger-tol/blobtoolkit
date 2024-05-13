7f43c52bff1a70a3e1c9c663fea1d9e81d0f5ff5
Matthieu Muffato <mm49@sanger.ac.uk>
2024-05-13 11:26:48 +0100

    Adding a parameter to choose the busco lineages

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -7,6 +7,14 @@ and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0
 
 The pipeline has now been validated for draft (unpublished) assemblies.
 
+### Parameters
+
+| Old parameter | New parameter    |
+| ------------- | ---------------- |
+|               | --busco_lineages |
+
+> **NB:** Parameter has been **updated** if both old and new parameter information is present. </br> **NB:** Parameter has been **added** if just the new parameter information is present. </br> **NB:** Parameter has been **removed** if new parameter information isn't present.
+
 ### Software dependencies
 
 Note, since the pipeline is using Nextflow DSL2, each process will be run with its own [Biocontainer](https://biocontainers.pro/#/registry). This means that on occasion it is entirely possible for the pipeline to be using different versions of the same tool. However, the overall software dependency changes compared to the last release have been listed below for reference. Only `Docker` or `Singularity` containers are supported, `conda` is not supported.
--- bin/get_odb_taxon.py
+++ bin/get_odb_taxon.py
@@ -16,7 +16,8 @@ def parse_args(args=None):
     parser.add_argument("TAXON_QUERY", help="Query string/integer for this taxon.")
     parser.add_argument("LINEAGE_TAX_IDS", help="Mapping between BUSCO lineages and taxon IDs.")
     parser.add_argument("FILE_OUT", help="Output CSV file.")
-    parser.add_argument("--version", action="version", version="%(prog)s 1.0")
+    parser.add_argument("--busco", dest="REQUESTED_BUSCOS", help="Requested BUSCO lineages.", default=None)
+    parser.add_argument("--version", action="version", version="%(prog)s 1.1")
     return parser.parse_args(args)
 
 
@@ -25,23 +26,34 @@ def make_dir(path):
         os.makedirs(path, exist_ok=True)
 
 
-def get_odb(taxon_query, lineage_tax_ids, file_out):
+def get_odb(taxon_query, lineage_tax_ids, requested_buscos, file_out):
     # Read the mapping between the BUSCO lineages and their taxon_id
     with open(lineage_tax_ids) as file_in:
         lineage_tax_ids_dict = {}
         for line in file_in:
             arr = line.split()
-            lineage_tax_ids_dict[int(arr[0])] = arr[1]
+            lineage_tax_ids_dict[int(arr[0])] = arr[1] + "_odb10"
 
     # Using API, get the taxon_ids of the species and all parents
     response = requests.get(NCBI_TAXONOMY_API % taxon_query).json()
-    this_taxon_id = response["taxonomy_nodes"][0]["taxonomy"]["tax_id"]
-    ancestor_taxon_ids = response["taxonomy_nodes"][0]["taxonomy"]["lineage"]
 
-    # Do the intersection to find the ancestors that have a BUSCO lineage
-    odb_arr = [
-        lineage_tax_ids_dict[taxon_id] for taxon_id in reversed(ancestor_taxon_ids) if taxon_id in lineage_tax_ids_dict
-    ]
+    this_taxon_id = response["taxonomy_nodes"][0]["taxonomy"]["tax_id"]
+
+    if requested_buscos:
+        odb_arr = requested_buscos.split(",")
+        valid_odbs = set(lineage_tax_ids_dict.values())
+        for odb in odb_arr:
+            if odb not in valid_odbs:
+                print(f"Invalid BUSCO lineage: {odb}", file=sys.stderr)
+                sys.exit(1)
+    else:
+        # Do the intersection to find the ancestors that have a BUSCO lineage
+        ancestor_taxon_ids = response["taxonomy_nodes"][0]["taxonomy"]["lineage"]
+        odb_arr = [
+            lineage_tax_ids_dict[taxon_id]
+            for taxon_id in reversed(ancestor_taxon_ids)
+            if taxon_id in lineage_tax_ids_dict
+        ]
 
     out_dir = os.path.dirname(file_out)
     make_dir(out_dir)
@@ -49,12 +61,12 @@ def get_odb(taxon_query, lineage_tax_ids, file_out):
     with open(file_out, "w") as fout:
         print("taxon_id", this_taxon_id, sep=",", file=fout)
         for odb_val in odb_arr:
-            print("busco_lineage", odb_val + "_odb10", sep=",", file=fout)
+            print("busco_lineage", odb_val, sep=",", file=fout)
 
 
 def main(args=None):
     args = parse_args(args)
-    get_odb(args.TAXON_QUERY, args.LINEAGE_TAX_IDS, args.FILE_OUT)
+    get_odb(args.TAXON_QUERY, args.LINEAGE_TAX_IDS, args.REQUESTED_BUSCOS, args.FILE_OUT)
 
 
 if __name__ == "__main__":
